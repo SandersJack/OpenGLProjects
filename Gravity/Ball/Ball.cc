db5471d6b33bd2d53b4c92107870c6e567b0324e
JackLaptop <sandersjack141@gmail.com>
2023-12-06 14:56:18 +0000

    added text to a second window that contains a slider that controls the damping of the collsions with the walls

--- Gravity/Ball/Ball.cc
+++ Gravity/Ball/Ball.cc
@@ -5,6 +5,7 @@
 #include <vector>
 #include <random>
 #include <algorithm>
+#include <map>
 
 #include <GL/glew.h>
 
@@ -14,7 +15,12 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtx/norm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 using namespace glm;
+
+#include <ft2build.h>
+#include FT_FREETYPE_H
 
 #include "LoadShaders.hh"
 #include "Controls.hh"
@@ -221,8 +227,14 @@ void initSliderWindow() {
         return;
     }
 
+	// Set the position of the slider window
+    glfwSetWindowPos(sliderWindow, 1200, 200);
+
     // Make the window's context current
     glfwMakeContextCurrent(sliderWindow);
+
+    // Dark blue background
+	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
 
     // Set GLFW mouse button callback
     glfwSetMouseButtonCallback(sliderWindow, mouse_button_callback);
@@ -235,16 +247,182 @@ void cleanupSliderWindow() {
     glfwTerminate();
 }
 
+
+
+GLuint textureID_text;
+GLuint textshaderProgram;
+GLuint textProgram2D;
+GLuint VAO_text, VBO_text;
+
+struct Character {
+    unsigned int TextureID;  // ID handle of the glyph texture
+    glm::ivec2   Size;       // Size of glyph
+    glm::ivec2   Bearing;    // Offset from baseline to left/top of glyph
+    long int Advance;    // Offset to advance to next glyph
+};
+
+std::map<char, Character> Characters;
+
+void initFreeType(const char* fontPath, int fontSize) {
+    // FreeType
+    // --------
+    FT_Library ft;
+    // All functions return a value different than 0 whenever an error occurred
+    if (FT_Init_FreeType(&ft))
+    {
+        std::cout << "ERROR::FREETYPE: Could not init FreeType Library" << std::endl;
+        
+    }
+
+	// find path to font
+    std::string font_name = "/home/jack/Code/Forces/Gravity/Ball/fonts/Roboto-Regular.ttf";
+    if (font_name.empty())
+    {
+        std::cout << "ERROR::FREETYPE: Failed to load font_name" << std::endl;
+        
+    }
+	
+	// load font as face
+    FT_Face face;
+    if (FT_New_Face(ft, font_name.c_str(), 0, &face)) {
+        std::cout << "ERROR::FREETYPE: Failed to load font" << std::endl;
+        
+    }
+    else {
+        // set size to load glyphs as
+        FT_Set_Pixel_Sizes(face, 0, 48);
+
+        // disable byte-alignment restriction
+        glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
+        // load first 128 characters of ASCII set
+        for (unsigned char c = 0; c < 128; c++)
+        {
+            // Load character glyph 
+            if (FT_Load_Char(face, c, FT_LOAD_RENDER))
+            {
+                std::cout << "ERROR::FREETYTPE: Failed to load Glyph" << std::endl;
+                continue;
+            }
+            // generate texture
+            unsigned int texture;
+            glGenTextures(1, &texture);
+            glBindTexture(GL_TEXTURE_2D, texture);
+            glTexImage2D(
+                GL_TEXTURE_2D,
+                0,
+                GL_RED,
+                face->glyph->bitmap.width,
+                face->glyph->bitmap.rows,
+                0,
+                GL_RED,
+                GL_UNSIGNED_BYTE,
+                face->glyph->bitmap.buffer
+            );
+            // set texture options
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+            // now store character for later use
+            Character character = {
+                texture,
+                glm::ivec2(face->glyph->bitmap.width, face->glyph->bitmap.rows),
+                glm::ivec2(face->glyph->bitmap_left, face->glyph->bitmap_top),
+                static_cast<unsigned int>(face->glyph->advance.x)
+            };
+            Characters.insert(std::pair<char, Character>(c, character));
+        }
+        glBindTexture(GL_TEXTURE_2D, 0);
+    }
+    // destroy FreeType once we're finished
+    FT_Done_Face(face);
+    FT_Done_FreeType(ft);
+}
+
+void RenderText(std::string text, float x, float y, float scale, glm::vec3 color) {
+    checkGLError("v");
+    glUseProgram(textProgram2D);
+    checkGLError("t");
+    glUniform3f(glGetUniformLocation(textProgram2D, "textColor"), color.x, color.y, color.z);
+    glActiveTexture(GL_TEXTURE0);
+    glBindVertexArray(VAO_text);
+
+    // iterate through all characters
+    std::string::const_iterator c;
+    for (c = text.begin(); c != text.end(); c++) 
+    {
+        Character ch = Characters[*c];
+
+        float xpos = x + ch.Bearing.x * scale;
+        float ypos = y - (ch.Size.y - ch.Bearing.y) * scale;
+
+        float w = ch.Size.x * scale;
+        float h = ch.Size.y * scale;
+        // update VBO for each character
+        float vertices[6][4] = {
+            { xpos,     ypos + h,   0.0f, 0.0f },            
+            { xpos,     ypos,       0.0f, 1.0f },
+            { xpos + w, ypos,       1.0f, 1.0f },
+
+            { xpos,     ypos + h,   0.0f, 0.0f },
+            { xpos + w, ypos,       1.0f, 1.0f },
+            { xpos + w, ypos + h,   1.0f, 0.0f }           
+        };
+        checkGLError("o");
+        // render glyph texture over quad
+        glBindTexture(GL_TEXTURE_2D, ch.TextureID);
+        checkGLError("TextureID");
+        // update content of VBO memory
+        glBindBuffer(GL_ARRAY_BUFFER, VBO_text);
+        glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices); // be sure to use glBufferSubData and not glBufferData
+
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        // render quad
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        // now advance cursors for next glyph (note that advance is number of 1/64 pixels)
+        x += (ch.Advance >> 6) * scale; // bitshift by 6 to get value in pixels (2^6 = 64 (divide amount of 1/64th pixels by 64 to get amount of pixels))
+    }
+    glBindVertexArray(0);
+    glBindTexture(GL_TEXTURE_2D, 0);
+}
+
+
 void drawSliderWindow() {
 	glfwMakeContextCurrent(sliderWindow);
 	glClear(GL_COLOR_BUFFER_BIT);
+    glEnable(GL_CULL_FACE);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA); 
 
 	drawSlider();
+    glUseProgram(textProgram2D);
+    glm::mat4 projection = glm::ortho(0.0f, static_cast<float>(400), 0.0f, static_cast<float>(200));
+    glUniformMatrix4fv(glGetUniformLocation(textProgram2D, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
+    RenderText("Damping", -.9f, 0.5f, 0.01f, glm::vec3(0.5, 0.8f, 0.2f));
 
 	glfwSwapBuffers(sliderWindow);
 	glfwPollEvents();
 }
 
+void initText(){
+
+    initFreeType("fonts/Roboto-Regular.ttf",48);
+    textProgram2D = LoadShaders( "Shaders/text.vs", "Shaders/text.fs" );
+    glGenVertexArrays(1, &VAO_text);
+    glGenBuffers(1, &VBO_text);
+    
+    glBindVertexArray(VAO_text);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO_text);
+    
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 6 * 4, NULL, GL_DYNAMIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(float), 0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);   
+    
+
+}
 
 
 template <typename T> int sgn(T val) {
@@ -260,8 +438,11 @@ int main() {
     std::uniform_real_distribution<float> Distribution_11(-1.0f, 1.0f);
 	std::uniform_real_distribution<float> Distribution_1010(-10.f, 10.f);
 
+    int SCR_WIDTH = 1024;
+    int SCR_HEIGHT = 768;
+
 	OGLManager *oglM = OGLManager::GetInstance();
-	oglM->Init(1024, 768);
+	oglM->Init(SCR_WIDTH, SCR_HEIGHT);
 
 	GLFWwindow *window = oglM->GetWindow();
 
@@ -359,15 +540,17 @@ int main() {
 	control->SetEnableUpDown(false);
 	control->SetEnableLeftRight(false);
 
-	
-	initSliderWindow();
+    initSliderWindow();
 	init_slider();
+
+    initText();
 	
 	//double damping = 0.9	;
 	glm::vec3 halfboundsize = glm::vec3(0.65 * 28 - 2*radius, 0.5 * 28 - 2*radius, 0.0f); 
 
 	checkGLError("control");
 	int spawnedBalls = 0;
+    int q = 0;
 	do {
 		glfwMakeContextCurrent(window);
 		// Clear the screen
@@ -375,8 +558,7 @@ int main() {
 		
 		glEnable(GL_DEPTH_TEST);
 
-		
-
+    
 		control->computeMatricesFromInputs();
 		glm::mat4 ProjectionMatrix = control->getProjectionMatrix();
 		glm::mat4 ViewMatrix = control->getViewMatrix();
@@ -385,7 +567,7 @@ int main() {
 		glm::mat4 ViewProjectionMatrix = ProjectionMatrix * ViewMatrix;
 
 		glm::mat4 ViewProjectionMatrix2 = ProjectionMatrix * ViewMatrix;
-
+        
 		shapeTools->render3DShape(ShapeshaderProgram, VAO, ViewProjectionMatrix, glm::vec3(0.0f, 0.0f, -30.0f), 12);
 		
 		double currentTime = glfwGetTime();
