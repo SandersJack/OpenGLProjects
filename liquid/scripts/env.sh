75858dfdf43f3a5704f6f8ae0034bb8ed793b212
JackLaptop <sandersjack141@gmail.com>
2023-12-17 16:23:54 +0000

    started liquid from the bll dir

---
+++ liquid/scripts/env.sh
@@ -0,0 +1,10 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR=$(dirname "$(dirname "${DIR}")")
+
+mkdir -p build
+
+alias make_clean="rm build/* -r"
+
+alias make="cmake -S . -B build; cmake --build build"
+
+alias Liquid="./build/Liquid"
