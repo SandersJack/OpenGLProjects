69c392c810d0818b3809d3c94b2652f225d880bc
JServer <sandersjack141@gmail.com>
2023-12-21 18:07:30 +0000

    Added the other folders

---
+++ CI/testbuild_Games.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Start of test build of Game Projects"
+
+cd Games/Breakout/
+
+shopt -s expand_aliases
+source scripts/env.sh
+
+make
---
+++ CI/testbuild_Gravity.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Start of test build of Gravity Projects"
+
+cd Gravity/Ball/
+
+shopt -s expand_aliases
+source scripts/env.sh
+
+make
